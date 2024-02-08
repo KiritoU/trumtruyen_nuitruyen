423d21f86b1ce0bc1ab17900e39e9bfe9ed7d0ff
KiritoU <nguyentienduongtink21@gmail.com>
2024-02-08 13:34:02 +0700

    FIX: crawled chapters slug

--- nuitruyen.py
+++ nuitruyen.py
@@ -29,14 +29,10 @@ class Nuitruyen:
             condition=f'post_parent="{comic_id}" AND post_type="chap"',
         )
 
-        chapter = chapters[0]
-        for i, field_value in enumerate(chapter):
-            print(i, field_value)
-
         chapters_slug = [
-            chapter[CONFIG.INSERT["posts"].index("post_name")] for chapter in chapters
+            chapter[CONFIG.INSERT["posts"].index("post_name") + 1]
+            for chapter in chapters
         ]
-        print(f"[+] Crawled chapters: {chapters_slug}")
 
         return chapters_slug
 
